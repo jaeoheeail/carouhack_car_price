65c8ef281713ebfef0957a4c4bfd67eaa9fc9970
Joel Foo <joel.foo@thecarousell.com>
2018-11-29 02:30:53 +0800

    add service

---
+++ predict_service/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask.ext import restful
+from flask import make_response
+from bson.json_util import dumps
+
+app = Flask(__name__)
+
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
+api = restful.Api(app)
+api.representations = DEFAULT_REPRESENTATIONS
+
+import predict_service.resources
---
+++ runserver.py
@@ -0,0 +1,2 @@
+from predict_service import app
+app.run(debug=True)
